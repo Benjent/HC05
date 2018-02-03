71f78805d7292d5e32a2e7aa75b815d460155434
benjent <Benjent@hotmail.fr>
2018-02-03 14:18:43 +0100

    Create new main activity that leads to MeteoActivity

--- app/src/main/java/fr/enssat/morvan/hc_05/MainActivity.java
+++ app/src/main/java/fr/enssat/morvan/hc_05/MainActivity.java
@@ -1,8 +1,8 @@
 package fr.enssat.morvan.hc_05;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -23,42 +23,23 @@ public class MainActivity extends AppCompatActivity {
         // Views
         buttonView = findViewById(R.id.ButtonView);
 
-        // Command manager
-        /*CommandManager commandManager = new CommandManager();
-        final String[] commands = {"temperature", "luminositee", "vent"};
-        for(String command : commands) {
-            commandManager.addCommand(command);
-        }
-        commandManager.addCommand()*/
+        Button button = new Button(this);
+        button.setTag("TODO");
+        button.setText("Go to meteo");
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String tag = (String) v.getTag();
+                // Create an Intent to MeteoActivity
+                Intent myIntent = new Intent(MainActivity.this, MeteoActivity.class);
 
-        // Commands
-        Command CmdTemperature  = new Command("temperature", "Température");
-        Command CmdLuminosity   = new Command("luminositee", "Luminositée");
-        Command CmdWindSpeed    = new Command("vent", "Vent");
-        Command[] commandList = {CmdTemperature, CmdLuminosity, CmdWindSpeed};
+                // Put the VideoMedata object as an extra
+                //myIntent.putExtra(VIDEO_METADATA_MESSAGE, videoMetadata);
 
-        for(Command command : commandList) {
-            Button button = new Button(this);
-            button.setTag(command.getCommand());
-            button.setText(command.getLabel());
-            button.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    final String tag = (String) v.getTag();
-                    sendCommand(tag);
-                }
-            });
-            buttonView.addView(button);
-        }
-
-    }
-
-    void sendCommand(String command) {
-        Log.v("Command","Sending command " + command + "...");
-        /*Button button = new Button(this);
-        button.setTag("HHH");
-        button.setText("HHH");
-        buttonView.addView(button);*/
-        // TODO use bluetooth to send to terminal
+                // Start the VideoViewActivity
+                startActivity(myIntent);
+            }
+        });
+        buttonView.addView(button);
     }
 }
---
+++ app/src/main/java/fr/enssat/morvan/hc_05/MeteoActivity.java
@@ -0,0 +1,69 @@
+package fr.enssat.morvan.hc_05;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+/**
+ * Created by benjent on 03/02/18.
+ */
+
+public class MeteoActivity extends AppCompatActivity {
+
+    LinearLayout buttonView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meteo);
+
+        // Get VideoMetadata object from the Intent
+        Intent intent = getIntent();
+        //videoMetadata = (VideoMetadata) intent.getParcelableExtra(MainActivity.VIDEO_METADATA_MESSAGE);
+
+        // Views
+        buttonView = findViewById(R.id.ButtonView);
+
+        // Command manager
+        /*CommandManager commandManager = new CommandManager();
+        final String[] commands = {"temperature", "luminositee", "vent"};
+        for(String command : commands) {
+            commandManager.addCommand(command);
+        }
+        commandManager.addCommand()*/
+
+        // Commands
+        Command CmdTemperature  = new Command("temperature", "Température");
+        Command CmdLuminosity   = new Command("luminositee", "Luminositée");
+        Command CmdWindSpeed    = new Command("vent", "Vent");
+        Command[] commandList = {CmdTemperature, CmdLuminosity, CmdWindSpeed};
+
+        for(Command command : commandList) {
+            Button button = new Button(this);
+            button.setTag(command.getCommand());
+            button.setText(command.getLabel());
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    final String tag = (String) v.getTag();
+                    sendCommand(tag);
+                }
+            });
+            buttonView.addView(button);
+        }
+
+    }
+
+    void sendCommand(String command) {
+        Log.v("Command","Sending command " + command + "...");
+        /*Button button = new Button(this);
+        button.setTag("HHH");
+        button.setText("HHH");
+        buttonView.addView(button);*/
+        // TODO use bluetooth to send to terminal
+    }
+}
